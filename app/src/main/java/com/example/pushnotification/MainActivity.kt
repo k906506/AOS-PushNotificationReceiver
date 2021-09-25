bdb5f7e4fa9e472fd452b99dc2d294cdda50791d
k906506 <k906506@naver.com>
2021-09-25 22:02:18 +0900

    프로퍼티 추가

--- app/src/main/java/com/example/pushnotification/MainActivity.kt
+++ app/src/main/java/com/example/pushnotification/MainActivity.kt
@@ -2,8 +2,17 @@ package com.example.pushnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    private val firebaseToken: TextView by lazy {
+        findViewById(R.id.firebaseTokenTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
