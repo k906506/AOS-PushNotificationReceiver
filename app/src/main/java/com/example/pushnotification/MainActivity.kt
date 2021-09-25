9cc83d7bb0731a7027e7615b2661db2e15dd0ba7
k906506 <k906506@naver.com>
2021-09-25 23:11:07 +0900

    토큰 출력을 위한 메소드 구현

--- app/src/main/java/com/example/pushnotification/MainActivity.kt
+++ app/src/main/java/com/example/pushnotification/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pushnotification
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     private val resultTextView: TextView by lazy {
@@ -16,5 +17,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseToken.text = task.result
+                }
+            }
     }
 }
